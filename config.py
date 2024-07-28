d8dcc2ae2d022ac6f66f2375dd7b72be92a4ff4f
Bhimraj Yadav <bhimrajyadav977@gmail.com>
2024-07-28 12:57:15 +0000

    adds chat template

---
+++ config.py
@@ -0,0 +1,18 @@
+CHAT_TEMPLATE = """
+{%- for message in messages %}
+    {%- set prefix = '<|begin_of_text|>' if loop.index0==0 else '' %}
+    {{- prefix + '<|start_header_id|>'+message['role']+'<|end_header_id|>\n\n' -}}
+    {%- if message['role'] == 'assistant' and 'tool_calls' in message %}
+        {%- for tool in message['tool_calls'] %}
+            {%- set tool_json = {'id': tool['id'], 'name': tool['function']['name'], 'arguments': tool['function']['arguments']} %}
+            {{- tool_json }}
+        {%- endfor %}
+        {{- '<|eot_id|>\n' }}
+    {%- else %}
+        {{- message['content'] + '<|eot_id|>\n' }}
+    {%- endif %}
+{%- endfor %}
+{%- if add_generation_prompt %}
+    {{- '<|start_header_id|>assistant<|end_header_id|>\n\n' }}
+{%- endif %}
+"""
